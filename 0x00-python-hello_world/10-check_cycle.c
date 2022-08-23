932b57fc3daa505bcc822706e59d596f049e3692
Swittee Franca <190401022@live.unilag.edu.ng>
2022-08-23 15:58:05 +0100

    py

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+int check_cycle(listint_t *list)
+{
+	listint_t *next;
+	listint_t *new;
+	int *p;
+	int i = 0;
+	int c;
+
+	next = malloc(sizeof(listint_t));
+	new = malloc(sizeof(listint_t));
+	next = list;
+
+	while(list != NULL)
+	{
+		new = list;
+		list = list->next;
+		while(new && new != list)
+		{
+			new = new->next;
+			if (new == next)
+			{
+				return (1);
+			}
+		}
+		if(list == next)
+		{
+			return (1);
+		}
+		next = list;
+	}
+	
+	return (0);
+
+}
