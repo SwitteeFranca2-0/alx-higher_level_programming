7c783ef84f4827c2e3e08b02a4ef09e2034054e5
Swittee Franca <190401022@live.unilag.edu.ng>
2022-08-27 00:07:34 +0100

    py

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,13 +2,22 @@
 
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_b) < 2 or len(tuple_a) < 2:
-        if not tuple_a[0]:
-            tuple_a[0] = 0
-        if not tuple_b[0]:
-            tuple_b[0] = 0
-        if not tuple_b[1]:
-            tuple_b[1] = 0
-        if not tuple_a[1]:
-            tuple_a[1] = 0
-    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    if (len(tuple_a) == 1):
+        a_2 = 0
+        a_1 = tuple_a[0]
+    elif (len(tuple_a) == 0):
+        a_1 = 0
+        a_2 = 0
+    else:
+        a_1 = tuple_a[0]
+        a_2 = tuple_a[1]
+    if (len(tuple_b) == 1):
+        b_2 = 0
+        b_1 = tuple_b[0]
+    elif (len(tuple_b) == 0):
+        b_1 = 0
+        b_2 = 0
+    else:
+        b_1 = tuple_b[0]
+        b_2 = tuple_b[1]
+    return (a_1 + b_1, a_2 + b_2)
