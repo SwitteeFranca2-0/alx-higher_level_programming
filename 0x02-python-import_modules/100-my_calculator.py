edd8a64270bd9be84ae73fb77527f94b9234a5f7
Swittee Franca <190401022@live.unilag.edu.ng>
2022-08-26 02:10:02 +0100

    py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     from sys import argv
 
     if (len(argv) != 4):
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>.")
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
 
     a = agrv[1]
--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,12 +2,14 @@
 
 if __name__ == "__main__":
     """Print arguments."""
-    import sys
+    from sys import argv
 
-    if (len(sys.argv) != 1):
-        print("{} arguments:".format(len(sys.argv)))
-        for arg in sys.argv:
-            if (sys.argv.index(arg) != 0):
-                print("{}: {}".format(sys.argv.index(arg), arg))
+    if (len(argv) > 2):
+        print("{} arguments:".format(len(argv) - 1))  
+    elif (len(argv) == 2):
+        print("1 argument.")
     else:
         print("0 arguments.")
+    for arg in argv:
+        if (argv.index(arg) != 0):
+            print("{}: {}".format(argv.index(arg), arg))
