107e79bd14d50d4b5c50251ef367e1afd6a87ffa
Swittee Franca <190401022@live.unilag.edu.ng>
2022-08-28 00:30:54 +0100

    py

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,10 +2,7 @@
 
 
 def max_integer(my_list=[]):
-    max = 0
     if len(my_list) == 0:
         return None
-    for num in my_list:
-        if num > max:
-            max = num
-    return max
+    my_list.sort()
+    return my_list[-1]
