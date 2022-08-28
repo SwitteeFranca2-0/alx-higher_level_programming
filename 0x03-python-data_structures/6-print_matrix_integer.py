a4741fc1091070fb601961a4122bf1f12e4e4e66
Swittee Franca <190401022@live.unilag.edu.ng>
2022-08-28 21:14:46 +0100

    py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,8 +4,8 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for num in row:
-           # if(row.index(num) == len(row) - 1 and len(row) != 0):
-            #    print("{}".format(num), end="")
-             #   continue
-            print("{}".format(num), end=" ")
+            if(row.index(num) == len(row) - 1 and len(row) != 0):
+                print("{:d}".format(num), end="")
+                continue
+            print("{:d}".format(num), end=" ")
         print("")
