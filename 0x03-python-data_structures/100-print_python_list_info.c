9730fb49a252caf662d9f5e7a6f46c0e52910831
Swittee Franca <190401022@live.unilag.edu.ng>
2022-08-29 17:52:50 +0100

    py

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+#include <object.h>
+#include <listobject.h>
+
+
+/**
+ * print_python_list_info - print info.
+ * @p: list
+ */
+void print_python_list_info(PyObject *p)
+{
+	Py_ssize_t len  = PyList_Size(p), i;
+
+	printf("[*] Size of the python List = %lu\n", len);
+	printf("[*] Allocated = %li\n", len)
+
+	for (i = 0, i < len; i++)
+	{
+		printf("Element %li: %s\n", i, (((PyObject *)(p))->ob_type));
+	}
+}
