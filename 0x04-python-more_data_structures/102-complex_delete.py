a15f4e9a6afc83979e625cb6c55f52ca263b0fa9
Swittee Franca <190401022@live.unilag.edu.ng>
2022-08-31 23:33:30 +0100

    py

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 
+
 def complex_delete(a_dictionary, value):
-	if len(a_dictionary) != 0:
-		list_1 = [keys for keys in a_dictionary if a_dictionary.get(keys) == value]
-		for key in list_1:
-			if key in a_dictionary:
-				del a_dictionary[key]
-	return a_dictionary
+    a = a_dictionary
+    if len(a_dictionary) != 0:
+        list_1 = [keys for keys in a if a.get(keys) == value]
+        for key in list_1:
+            if key in a_dictionary:
+                del a_dictionary[key]
+    return a_dictionary
