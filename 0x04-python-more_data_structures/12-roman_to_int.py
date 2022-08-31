9673d426db806fd100856fa3764d584b2f8c5c06
Swittee Franca <190401022@live.unilag.edu.ng>
2022-08-31 21:51:56 +0100

    py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,10 +2,13 @@
 
 
 def roman_to_int(roman_string):
-    r = roman_string
     i = 0
     num = 0
     r_num = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+
+    if not roman_string or type(roman_string) != str:
+        return 0
+    r = roman_string
     while(i < len(r)):
         if i + 1 == len(r):
             num += r_num.get(r[i])
