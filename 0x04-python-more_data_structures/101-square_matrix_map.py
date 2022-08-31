4b401db8f1d79906f1a0c7f7bcd8303992ff695a
Swittee Franca <190401022@live.unilag.edu.ng>
2022-09-01 00:08:54 +0100

    py

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-	return map(lambda x: x * x, matrix[x])
+    return list(map(lambda x: list(map(lambda p: p*p, x[:])), matrix))
---
+++ 0x04-python-more_data_structures/103-python.c
@@ -0,0 +1,6 @@
+#include <Python.h>
+#include <object.h>
+#include <listobject.h>
+
+void print_python_list(PyObject *p):
+	
