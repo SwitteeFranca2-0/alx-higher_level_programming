a2c876c1943f241c94bbcb04d83f9dac62dc09eb
Swittee Franca <190401022@live.unilag.edu.ng>
2022-09-01 19:10:28 +0100

    py

--- 0x17-doubly_linked_lists/1-distint_len.c
+++ 0x17-doubly_linked_lists/1-distint_len.c
@@ -4,20 +4,18 @@
 #include "lists.h"
 
 /**
- * print_dlistint - print values.
+ * dlistint_len - length.
  * @h: h.
  * Return: size_t
  */
-size_t print_dlistint(const dlistint_t *h)
+size_t dlistint_len(const dlistint_t *h)
 {
 	size_t i = 0;
 
-
 	while (h)
 	{
-		printf("%d\n", h->n);
-		h = h->next;
 		i++;
+		h = h->next;
 	}
 
 	return (i);
---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t 
+}
