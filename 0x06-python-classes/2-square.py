7e1039e44998ab7432a81b343e6cd682b1353340
Swittee Franca <190401022@live.unilag.edu.ng>
2022-09-14 16:52:15 +0100

    OOP

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
-"""Define a class Square."""
-
+"""Defining a square class"""
 
 class Square:
-    """Represent a square."""
+    """
+        class defining a square.
+        """
 
     def __init__(self, size=0):
-        """Initialize a new Square.
+        """Initializig the size and including sme conditionals
 
         Args:
-            size (int): The size of the created member of classsquare.
-        """
-        if not isinstance(size, int):
+            size(int): size of the square."""
+
+        if type(size) is not int:
             raise TypeError("size must be an integer")
-        elif size < 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
