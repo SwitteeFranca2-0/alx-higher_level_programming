051c7f121f6b96ec32868013db864e6e8f6cc891
Swittee Franca <190401022@live.unilag.edu.ng>
2022-09-20 17:03:13 +0100

    py

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """This module defines a class"""
 
+
 class LockedClass:
     """This is a locked class"""
 
     __slots__ = ['first_name']
+
     def __init__(self, first_name=""):
         """This function says a name
             Args:
