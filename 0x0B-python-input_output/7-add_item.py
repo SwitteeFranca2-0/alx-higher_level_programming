d9588082c80e696674199f0465c3ff32b2684829
Swittee Franca <190401022@live.unilag.edu.ng>
2022-09-28 00:36:03 +0100

    i/o

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,8 +6,5 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-try:
-    js_list = load_from_json_file("add_item.json")
-except FileNotFoundError:
-    js_list = [i for i in argv if argv.index(i) != 0]
+js_list = [i for i in argv if argv.index(i) != 0]
 save_to_json_file(js_list, "add_item.json")
