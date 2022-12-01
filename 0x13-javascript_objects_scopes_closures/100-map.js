e3fd9eb2d457dcf374e14b0208ae00d254d0b5ba
Swittee Franca <190401022@live.unilag.edu.ng>
2022-12-01 05:50:08 +0100

    javascript

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,9 +1,4 @@
 #!/usr/bin/node
-
-const exList = require('./100-data').list;
-const newList = exlist.map(function (num, index) {
-  return num * index;
-})
-
-console.log(exlist);
-console.log(newList);
+const list = require('./100-data.js').list;
+console.log(list);
+console.log(list.map((item, index) => item * index));
