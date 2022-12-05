35ce6b721ccd3ab56ce4bc23b9cb3877e7c18cc3
Swittee Franca <190401022@live.unilag.edu.ng>
2022-12-05 02:24:31 +0100

    python  orm

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -7,7 +7,7 @@ import sys
 
 if __name__ == '__main__':
     conn = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                           passwd=sys.argv[2], b=sys.argv[3], port=3306)
+                           passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = conn.cursor()
     na = sys.argv[4]
     cur.execute("SELECT * FROM states WHERE name = %(na) s ORDER BY id ASC",
