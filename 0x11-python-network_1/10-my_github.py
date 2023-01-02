cf3d098e47c379be512240602dd79cecb9cf23f4
SwitteeFranca2-0 <switteefrankie@gmail.com>
2023-01-01 23:20:14 -0800

    Using python scripts to make url requests

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -7,5 +7,5 @@ from requests.auth import HTTPBasicAuth
 
 if __name__ == "__main__":
     auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
-    response = requests.get('https://api.github.com/user', auth=auth):wq
-
+    response = requests.get('https://api.github.com/user', auth=auth)
+    print(r.json().get("id"))
