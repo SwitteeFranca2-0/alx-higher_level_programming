b47178209f14871add4587e344117136a1a2df8c
SwitteeFranca2-0 <switteefrankie@gmail.com>
2023-01-01 23:42:58 -0800

    Using python scripts to make url requests

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""This is a post request to a url"""
+
+import requests
+import sys
+
+payload = {'email': sys.argv[2]}
+r = requests.post(sys.argv[1], data=payload)
+print(r.text)
