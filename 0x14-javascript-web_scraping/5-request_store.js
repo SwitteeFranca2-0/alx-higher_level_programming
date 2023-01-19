91503e6939100cec3e65779973ee9114ddfab218
Swittee Franca <190401022@live.unilag.edu.ng>
2023-01-19 12:57:29 +0100

    fixing errors according to semistandard rules

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -3,13 +3,12 @@
 const request = require('request');
 const fs = require('fs');
 
-let url = process.argv[2]
-let file = process.argv[3]
+const url = process.argv[2];
+const file = process.argv[3];
 
-request.get(url, function(error, response, body) {
-    if (error) throw error;
-    fs.writeFile(file, body,{encoding: "utf8"}, (err) => {
-        if (err) throw err;
-    })
-
-})
+request.get(url, function (error, response, body) {
+  if (error) throw error;
+  fs.writeFile(file, body, { encoding: 'utf8' }, (err) => {
+    if (err) throw err;
+  });
+});
